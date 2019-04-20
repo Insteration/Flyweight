aa5cebe04145c6aefd0798f5217e151cbcb999c3
Artem Karmaz <inste@icloud.com>
2019-04-20 13:24:13 +0300

    Initial Commit

---
+++ Flyweight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flyweight
+//
+//  Created by Artem Karmaz on 4/20/19.
+//  Copyright © 2019 Artem Karmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
